58b98a9b9decb180b1f357848df7cc3b119ace25
MD.MOHSIN <74142229+mohsin0176@users.noreply.github.com>
2021-08-21 13:46:51 +0600

    jquery autocomplete serach box implementation...

---
+++ Jquery AutoComplete/autocomplete.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>     
+<body>
+
+<h2>Jquery Auto Complete Search Box</h2>
+
+<!--Make sure the form has the autocomplete function switched off:-->
+<form autocomplete="off" action="/action_page.php">
+  <div class="autocomplete" style="width:300px;">
+    <input id="myInput" type="text" name="myCountry" placeholder="Country">
+  </div>
+  <input type="submit">
+</form>
+
+<script type="text/javascript" src="auto.js"></script>
+</body>
+</html>
